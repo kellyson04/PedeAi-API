2a733ec5f2ffcbab7e2a9ebf47b214047e677929
Kellyson <gilsonvigilante@outlook.com>
2026-06-08 10:00:08 -0300

    feat: iniciando o projeto

---
+++ src/main/java/dev/kellyson/projeto/PedeAi/API/PedeAiApiApplication.java
@@ -0,0 +1,13 @@
+package dev.kellyson.projeto.PedeAi.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedeAiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedeAiApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/kellyson/projeto/PedeAi/API/PedeAiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kellyson.projeto.PedeAi.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedeAiApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
