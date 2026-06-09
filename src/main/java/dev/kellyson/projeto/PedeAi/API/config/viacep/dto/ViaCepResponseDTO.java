5141dc09dbc02ab9df3bf4e576e55945ceff6693
Kellyson <gilsonvigilante@outlook.com>
2026-06-08 23:42:17 -0300

    feat: cria DTO pra pegar o PAYLOAD da api viacep

--- src/main/java/dev/kellyson/projeto/PedeAi/API/config/viacep/dto/ViaCepResponseDTO.java
+++ src/main/java/dev/kellyson/projeto/PedeAi/API/config/viacep/dto/ViaCepResponseDTO.java
@@ -1,4 +1,12 @@
 package dev.kellyson.projeto.PedeAi.API.config.viacep.dto;
 
-public record ViaCepResponseDTO() {
+public record ViaCepResponseDTO(
+        String cep,
+        String logradouro,
+        String complemento,
+        String bairro,
+        String localidade,
+        String uf,
+        Boolean erro
+) {
 }
