8a715098dd53e675f59aa78606cb0a64472ba508
Kellyson <gilsonvigilante@outlook.com>
2026-06-08 23:38:49 -0300

    feat: cria repositorio do user com metodos pra auxiliar no spring security e na validacao

--- src/main/java/dev/kellyson/projeto/PedeAi/API/user/repository/UserRepository.java
+++ src/main/java/dev/kellyson/projeto/PedeAi/API/user/repository/UserRepository.java
@@ -1,4 +1,13 @@
 package dev.kellyson.projeto.PedeAi.API.user.repository;
 
-public interface UserRepository {
+import dev.kellyson.projeto.PedeAi.API.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
 }
