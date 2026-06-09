aab7535d9d81b633d36949c3d5153531e360f611
Kellyson <gilsonvigilante@outlook.com>
2026-06-08 23:45:49 -0300

    feat: mapper pra response do user pra diminuir o boilerplate

---
+++ src/main/java/dev/kellyson/projeto/PedeAi/API/user/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package dev.kellyson.projeto.PedeAi.API.user.mapper;
+
+import dev.kellyson.projeto.PedeAi.API.user.entity.User;
+import dev.kellyson.projeto.PedeAi.API.user.dto.RegisterRequestDTO;
+import dev.kellyson.projeto.PedeAi.API.user.dto.UserResponseDTO;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class UserMapper {
+
+    public static UserResponseDTO toResponse(User user) {
+        return UserResponseDTO.builder()
+                .name(user.getName())
+                .createdAt(user.getCreatedAt())
+                .build();
+    }
+
+}
