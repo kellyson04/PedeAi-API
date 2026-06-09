2fad0a0c3e9d57b85e147c213ccfe6158ce5a3df
Kellyson <gilsonvigilante@outlook.com>
2026-06-08 23:39:44 -0300

    feat: cria classe de config pra integrar com a api viacep

--- src/main/java/dev/kellyson/projeto/PedeAi/API/config/viacep/ViaCepConfig.java
+++ src/main/java/dev/kellyson/projeto/PedeAi/API/config/viacep/ViaCepConfig.java
@@ -1,4 +1,25 @@
 package dev.kellyson.projeto.PedeAi.API.config.viacep;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestClient;
+
+import java.time.Duration;
+
+@Configuration
 public class ViaCepConfig {
+
+    @Bean
+    public RestClient viaCepRestClient() {
+        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+        requestFactory.setConnectTimeout(Duration.ofSeconds(2));
+        requestFactory.setReadTimeout(Duration.ofSeconds(5));
+
+        return RestClient.builder()
+                .baseUrl("https://viacep.com.br")
+                .requestFactory(requestFactory)
+                .build();
+    }
+
 }
