be79dd7b150f71cfc45ae471186ed47d97d67d26
Kellyson <gilsonvigilante@outlook.com>
2026-06-08 23:41:25 -0300

    feat: cria classe de servico pra autenticação com lógica de registro do usuario ja settada

--- src/main/java/dev/kellyson/projeto/PedeAi/API/auth/service/AuthService.java
+++ src/main/java/dev/kellyson/projeto/PedeAi/API/auth/service/AuthService.java
@@ -1,4 +1,46 @@
 package dev.kellyson.projeto.PedeAi.API.auth.service;
 
+import dev.kellyson.projeto.PedeAi.API.config.viacep.dto.ViaCepResponseDTO;
+import dev.kellyson.projeto.PedeAi.API.config.viacep.service.ViaCepService;
+import dev.kellyson.projeto.PedeAi.API.user.dto.LoginRequestDTO;
+import dev.kellyson.projeto.PedeAi.API.user.dto.RegisterRequestDTO;
+import dev.kellyson.projeto.PedeAi.API.user.dto.UserResponseDTO;
+import dev.kellyson.projeto.PedeAi.API.user.entity.User;
+import dev.kellyson.projeto.PedeAi.API.user.mapper.UserMapper;
+import dev.kellyson.projeto.PedeAi.API.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class AuthService {
+
+    private final UserRepository userRepository;
+    private final AuthenticationManager authenticationManager;
+    private final PasswordEncoder passwordEncoder;
+    private final ViaCepService viaCepService;
+
+    public UserResponseDTO register(RegisterRequestDTO registerRequestDTO) {
+        if (userRepository.existsByEmail(registerRequestDTO.email())) {
+            throw new RuntimeException("Email ja em uso");
+        }
+
+        ViaCepResponseDTO adress = viaCepService.findAdressByCep(registerRequestDTO.cep());
+
+        User user = User.builder()
+                .name(registerRequestDTO.name())
+                .email(registerRequestDTO.email())
+                .uf(adress.uf())
+                .city(adress.localidade())
+                .password(passwordEncoder.encode(registerRequestDTO.password()))
+                .build();
+
+        userRepository.save(user);
+
+        return UserMapper.toResponse(user);
+    }
+
+
 }
