87b25515184070e5be8872cfbb3af88a5889596e
Kellyson <gilsonvigilante@outlook.com>
2026-06-08 23:43:58 -0300

    feat: cria o impl de userdetailsservice pra carrega o user e buscar no banco antes de settar ele no contexto ou logar

--- src/main/java/dev/kellyson/projeto/PedeAi/API/auth/service/UserDetailsServiceImpl.java
+++ src/main/java/dev/kellyson/projeto/PedeAi/API/auth/service/UserDetailsServiceImpl.java
@@ -1,4 +1,21 @@
 package dev.kellyson.projeto.PedeAi.API.auth.service;
 
-public class UserDetailsServiceImpl {
+import dev.kellyson.projeto.PedeAi.API.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("Usuario não encontrado"));
+    }
+
 }
