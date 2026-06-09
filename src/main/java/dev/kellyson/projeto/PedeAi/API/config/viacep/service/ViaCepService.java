6ed8b32c252425f3b34a776ec48eed2ef9ab9900
Kellyson <gilsonvigilante@outlook.com>
2026-06-08 23:40:36 -0300

    feat: cria classe de serviço pra pegar o payload da viacep e validar se o cep existe

--- src/main/java/dev/kellyson/projeto/PedeAi/API/config/viacep/service/ViaCepService.java
+++ src/main/java/dev/kellyson/projeto/PedeAi/API/config/viacep/service/ViaCepService.java
@@ -1,4 +1,28 @@
 package dev.kellyson.projeto.PedeAi.API.config.viacep.service;
 
+import dev.kellyson.projeto.PedeAi.API.config.viacep.dto.ViaCepResponseDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+
+@Service
+@RequiredArgsConstructor
 public class ViaCepService {
+
+    private final RestClient viaCepRestClient;
+
+    public ViaCepResponseDTO findAdressByCep(String cep) {
+        ViaCepResponseDTO response = viaCepRestClient
+                .get()
+                .uri("/ws/{cep}/json/", cep)
+                .retrieve()
+                .body(ViaCepResponseDTO.class);
+
+        if (response == null || Boolean.TRUE.equals(response.erro())) {
+            throw new RuntimeException("Cep não encontrado");
+        }
+
+        return response;
+    }
+
 }
