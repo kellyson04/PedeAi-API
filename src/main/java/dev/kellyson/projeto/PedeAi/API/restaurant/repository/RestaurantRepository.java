e6cd7cd7354c776788f40d6bb836479704555ccc
Kellyson <gilsonvigilante@outlook.com>
2026-06-10 02:01:37 -0300

    feat: cria repositorio pra restaurant

--- src/main/java/dev/kellyson/projeto/PedeAi/API/restaurant/repository/RestaurantRepository.java
+++ src/main/java/dev/kellyson/projeto/PedeAi/API/restaurant/repository/RestaurantRepository.java
@@ -1,4 +1,15 @@
 package dev.kellyson.projeto.PedeAi.API.restaurant.repository;
 
-public interface RestaurantRepository {
+import dev.kellyson.projeto.PedeAi.API.restaurant.entity.Restaurant;
+import dev.kellyson.projeto.PedeAi.API.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface RestaurantRepository extends JpaRepository<Restaurant,Long> {
+    boolean existsByName(String name);
+    boolean existsByPhone(String phone);
+    List<Restaurant> findByOwner(User owner);
 }
