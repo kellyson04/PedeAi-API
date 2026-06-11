1f367eee026e8e73a4823f968528f0075718f564
Kell <gilsonvigilante@outlook.com>
2026-06-11 10:04:14 -0300

    feat: cria entidade de categoria

---
+++ src/main/java/dev/kellyson/projeto/PedeAi/API/category/entity/Category.java
@@ -0,0 +1,39 @@
+package dev.kellyson.projeto.PedeAi.API.category.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(name = "is_active", nullable = false)
+    private Boolean isActive;
+
+    @PrePersist
+    public void prePersist() {
+        this.isActive = true;
+    }
+}
