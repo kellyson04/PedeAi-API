18c55dfc55d3b0b2f8639e6302e91b1ddc34c6ed
Kell <gilsonvigilante@outlook.com>
2026-06-11 10:04:26 -0300

    feat: cria repositorio de categoria

---
+++ src/main/java/dev/kellyson/projeto/PedeAi/API/category/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package dev.kellyson.projeto.PedeAi.API.category.repository;
+
+import dev.kellyson.projeto.PedeAi.API.category.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
