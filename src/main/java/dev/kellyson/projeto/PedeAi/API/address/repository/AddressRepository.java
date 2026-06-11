cb56153af50eece8ed1e8ebc207cd703f8432cd2
Kell <gilsonvigilante@outlook.com>
2026-06-11 19:34:53 -0300

    feat: adiciona busca de enderecos por usuario

--- src/main/java/dev/kellyson/projeto/PedeAi/API/address/repository/AddressRepository.java
+++ src/main/java/dev/kellyson/projeto/PedeAi/API/address/repository/AddressRepository.java
@@ -1,9 +1,13 @@
 package dev.kellyson.projeto.PedeAi.API.address.repository;
 
 import dev.kellyson.projeto.PedeAi.API.address.entity.Address;
+import dev.kellyson.projeto.PedeAi.API.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AddressRepository extends JpaRepository<Address, Long> {
+    List<Address> findByUser(User user);
 }
