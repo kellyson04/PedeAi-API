b7a9c9cd981f37bd942e2fa375b048bddcd7910f
Kell <gilsonvigilante@outlook.com>
2026-06-11 23:17:19 -0300

    feat: adiciona busca de produtos disponiveis

--- src/main/java/dev/kellyson/projeto/PedeAi/API/product/repository/ProductRepository.java
+++ src/main/java/dev/kellyson/projeto/PedeAi/API/product/repository/ProductRepository.java
@@ -5,7 +5,10 @@ import dev.kellyson.projeto.PedeAi.API.restaurant.entity.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
     boolean existsByRestaurantAndNameIgnoreCase(Restaurant restaurant, String name);
+    List<Product> findByRestaurantAndIsAvailableTrue(Restaurant restaurant);
 }
