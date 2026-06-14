1331b7403444ec4a815bb3309d067536bf296cae
Kell <gilsonvigilante@outlook.com>
2026-06-14 15:05:00 -0300

    feat: cria endpoint de cadastrar endereco

--- src/main/java/dev/kellyson/projeto/PedeAi/API/address/controller/AddressController.java
+++ src/main/java/dev/kellyson/projeto/PedeAi/API/address/controller/AddressController.java
@@ -1,11 +1,16 @@
 package dev.kellyson.projeto.PedeAi.API.address.controller;
 
+import dev.kellyson.projeto.PedeAi.API.address.dto.AddressRequestDTO;
 import dev.kellyson.projeto.PedeAi.API.address.dto.AddressResponseDTO;
 import dev.kellyson.projeto.PedeAi.API.address.service.AddressService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +26,11 @@ public class AddressController {
     @GetMapping("/me")
     public ResponseEntity<List<AddressResponseDTO>> myAddresses(Authentication authentication) {
         return ResponseEntity.ok(addressService.myAddresses(authentication));
+    }
+
+    @PostMapping
+    public ResponseEntity<AddressResponseDTO> registerAddress(@RequestBody @Valid AddressRequestDTO request,
+                                                              Authentication authentication) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(addressService.registerAddress(request, authentication));
     }
 }
