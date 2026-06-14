f50f1b2193f043612f7f292c7d2d0acf54f0f37a
Kell <gilsonvigilante@outlook.com>
2026-06-14 15:04:47 -0300

    feat: cria logica de cadastrar endereco

--- src/main/java/dev/kellyson/projeto/PedeAi/API/address/service/AddressService.java
+++ src/main/java/dev/kellyson/projeto/PedeAi/API/address/service/AddressService.java
@@ -1,8 +1,12 @@
 package dev.kellyson.projeto.PedeAi.API.address.service;
 
+import dev.kellyson.projeto.PedeAi.API.address.dto.AddressRequestDTO;
 import dev.kellyson.projeto.PedeAi.API.address.dto.AddressResponseDTO;
+import dev.kellyson.projeto.PedeAi.API.address.entity.Address;
 import dev.kellyson.projeto.PedeAi.API.address.mapper.AddressMapper;
 import dev.kellyson.projeto.PedeAi.API.address.repository.AddressRepository;
+import dev.kellyson.projeto.PedeAi.API.config.viacep.dto.ViaCepResponseDTO;
+import dev.kellyson.projeto.PedeAi.API.config.viacep.service.ViaCepService;
 import dev.kellyson.projeto.PedeAi.API.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
@@ -16,6 +20,7 @@ import java.util.List;
 public class AddressService {
 
     private final AddressRepository addressRepository;
+    private final ViaCepService viaCepService;
 
     @Transactional(readOnly = true)
     public List<AddressResponseDTO> myAddresses(Authentication authentication) {
@@ -24,5 +29,27 @@ public class AddressService {
         return addressRepository.findByUser(user).stream()
                 .map(address -> AddressMapper.toResponse(address))
                 .toList();
+    }
+
+    @Transactional
+    public AddressResponseDTO registerAddress(AddressRequestDTO request, Authentication authentication) {
+        User user = (User) authentication.getPrincipal();
+
+        ViaCepResponseDTO addressData = viaCepService.findAdressByCep(request.cep());
+
+        Address address = Address.builder()
+                .user(user)
+                .street(addressData.logradouro())
+                .number(request.number())
+                .neighborhood(addressData.bairro())
+                .city(addressData.localidade())
+                .state(addressData.uf())
+                .zipCode(addressData.cep())
+                .complement(request.complement())
+                .build();
+
+        addressRepository.save(address);
+
+        return AddressMapper.toResponse(address);
     }
 }
