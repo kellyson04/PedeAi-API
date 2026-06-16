6987c6fab23970becc3476ce1d4dd6310841ffa0
Kell <gilsonvigilante@outlook.com>
2026-06-16 15:10:03 -0300

    feat: simplifica dto de criar pedido

--- src/main/java/dev/kellyson/projeto/PedeAi/API/order/dto/CreateOrderRequestDTO.java
+++ src/main/java/dev/kellyson/projeto/PedeAi/API/order/dto/CreateOrderRequestDTO.java
@@ -2,25 +2,8 @@ package dev.kellyson.projeto.PedeAi.API.order.dto;
 
 import jakarta.validation.constraints.NotNull;
 
-import java.math.BigDecimal;
-
 public record CreateOrderRequestDTO(
         @NotNull
-        Long restaurantId,
-
-        @NotNull
-        Long addressId,
-
-        @NotNull
-        BigDecimal subtotal,
-
-        @NotNull
-        BigDecimal deliveryFee,
-
-        @NotNull
-        BigDecimal discount,
-
-        @NotNull
-        BigDecimal total
+        Long addressId
 ) {
 }
